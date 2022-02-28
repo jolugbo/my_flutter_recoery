2c85fac5f703e34f739245838550566de2af9e6c
jolugbo olufemi <jolugbofemi@gmail.com>
2022-02-28 13:20:11 +0100

    work ongoing on splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,2 +1,26 @@
 import 'package:flutter/material.dart';
 
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+  @override
+ _SplashState createState() => _SplashState();
+
+
+}
+class _SplashState extends State<Splash>{
+  @override
+  void initState(){
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Container(
+              child: Text('Splash Screen',style: TextStyle(color: Colors.black,fontSize: 24,fontWeight: FontWeight.bold),)
+          ),)
+    );
+  }
+}
+
+
