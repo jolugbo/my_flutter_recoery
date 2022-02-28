4b33dd3592d5b198d63b58786a649363a4db9860
jolugbo olufemi <jolugbofemi@gmail.com>
2022-02-28 16:49:35 +0100

    completed work on the animated splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_recovery/pages/walk_through.dart';
+
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -11,14 +13,32 @@ class _SplashState extends State<Splash>{
   @override
   void initState(){
     super.initState();
+    //pageRedirect();
+  }
+  void pageRedirect() async {
+    await Future.delayed(Duration(microseconds: 1500),(){});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => WalkThroughPage(title: 'We are home',)));
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Container(
-              child: Text('Splash Screen',style: TextStyle(color: Colors.black,fontSize: 24,fontWeight: FontWeight.bold),)
-          ),)
+
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                height:100,
+                  width: 200,
+                  color: Colors.blue,
+              ),
+              Container(
+                  child: Text('Splash Screen',style: TextStyle(color: Colors.black,fontSize: 24,fontWeight: FontWeight.bold),)
+              ),
+            ],
+          ))
     );
   }
 }
